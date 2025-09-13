0caa1ce098ad9efee58b83c7fe9b8d6ed0d3b477
madsquarepants <ghazan.cena@gmail.com>
2025-09-12 23:31:25 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/",     // 👈 important for Vercel
+  base: "/",
   plugins: [react()],
 });
