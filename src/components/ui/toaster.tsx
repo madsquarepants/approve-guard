5e87ba48f3efe96a1ff973ffe6c57e4c4cc8e560
madsquarepants <ghazan.cena@gmail.com>
2025-09-13 00:27:38 -0700

    Update toaster.tsx

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,24 +1,10 @@
-import { useToast } from "@/hooks/use-toast";
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+// src/components/ui/toaster.tsx
+"use client";
 
+import { Toaster as SonnerToaster } from "sonner";
+
+// Named + default export (covers both import styles)
 export function Toaster() {
-  const { toasts } = useToast();
-
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && <ToastDescription>{description}</ToastDescription>}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  );
+  return <SonnerToaster richColors position="top-right" />;
 }
+export default Toaster;
