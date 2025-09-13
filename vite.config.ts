f55230a2623c703456aee42a5c3899d3d07eb1ae
madsquarepants <ghazan.cena@gmail.com>
2025-09-12 23:21:10 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
+// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: "/",     // 👈 important for Vercel
+  plugins: [react()],
+});
